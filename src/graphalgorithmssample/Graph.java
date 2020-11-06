057c702bcc2f0b72b12b860240937f6471e97339
zhuang002 <zhuang001@gmail.com>
2020-11-05 20:07:06 -0500

    scheleton

---
+++ src/graphalgorithmssample/Graph.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphalgorithmssample;
+
+/**
+ *
+ * @author zhuan
+ */
+public class Graph {
+    int[][] matrix=null;
+    public Graph(int[][] m) {
+        matrix=m;
+    }
+    
+    public void setMatrix(int[][] m) {
+        matrix=m;
+    }
+    
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    Graph prim() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    Graph kruskal() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
---
+++ src/graphalgorithmssample/GrpahAlgorithmsSample.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphalgorithmssample;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author zhuan
+ */
+public class GrpahAlgorithmsSample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[][] matrix=readInMatrix();
+        Graph graph=new Graph(matrix);
+        Graph tree=graph.prim();// tree=grph.kruskal();
+        matrix=tree.getMatrix();
+        printMetrix(matrix);
+    }
+
+    private static int[][] readInMatrix() {
+        Scanner sc=new Scanner(System.in);
+        int nodes=sc.nextInt();
+        int paths=sc.nextInt();
+        
+        int[][] matrix=new int[nodes][nodes];
+
+        for (int i=0;i<paths;i++) {
+            int node1=sc.nextInt();
+            int node2=sc.nextInt();
+            int length=sc.nextInt();
+            matrix[node1][node2]=length;
+            matrix[node2][node1]=length;
+        }
+        
+        return matrix;
+        
+    }
+
+    private static void printMetrix(int[][] matrix) {
+        for (int i=0;i<matrix.length;i++) {
+            for (int j=i;j<matrix.length;j++) {
+                if (matrix[i][j]!=0) {
+                    System.out.println(i+" "+j+" "+matrix[i][j]);
+                }
+            }
+        }
+    }
+
+}
+/*
+Possible tools:
+java.util.HashSet
+Collections.sort()
+Collections.binarySort()
+ArrayList
+
+*/
