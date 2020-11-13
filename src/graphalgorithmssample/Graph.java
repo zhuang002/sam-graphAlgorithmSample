f00d5e294309aa23f52b9fe1f2f9314147c05a8a
zhuang002 <zhuang001@gmail.com>
2020-11-12 19:22:54 -0500

    implement prim

--- src/graphalgorithmssample/Graph.java
+++ src/graphalgorithmssample/Graph.java
@@ -28,13 +28,13 @@ public class Graph {
 
     public Graph prim() {
         int[][] treeMatrix=new int[matrix.length][matrix.length];
-        ArrayList<Integer> includeSet=new ArrayList();
-        includeSet.add(0);
-        while (includeSet.size()<matrix.length) {
-            int[] newPath = getNearestPath(includeSet);
-            treeMatrix[newPath[0]][newPath[1]]=newPath[2];
-            treeMatrix[newPath[1]][newPath[0]]=newPath[2];
-            includeSet.add(newPath[1]);
+        ArrayList<Integer> includeSet=new ArrayList(); // the node set contains all nodes already included in the tree
+        includeSet.add(0); //initialize the included set with the first node;
+        while (includeSet.size()<matrix.length) { // if no all nodes are included;
+            int[] newPath = getNearestPath(includeSet); // get the nearest path that connect to the sub tree
+            treeMatrix[newPath[0]][newPath[1]]=newPath[2]; // expand the subtree with the new nearest node. set the originate node of the path
+            treeMatrix[newPath[1]][newPath[0]]=newPath[2]; // set the destiny node of the path
+            includeSet.add(newPath[1]); // add
         }
         return new Graph(treeMatrix);
         
@@ -45,7 +45,22 @@ public class Graph {
     }
 
     private int[] getNearestPath(ArrayList<Integer> includeSet) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int[] path=new int[3];
+        int min=Integer.MAX_VALUE;
+        for (Integer node : includeSet) {
+            for (int i=0;i<matrix.length;i++) {
+                int len=matrix[node][i];
+                if (len>0 && !includeSet.contains(i)) {
+                    if (min>len) {
+                        min=len;
+                        path[0]=node;
+                        path[1]=i;
+                        path[2]=len;
+                    }
+                }
+            }
+        }
+        return path;
     }
 
     
